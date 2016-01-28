0c1c0deda129236be78fbc30565755da86bf012f
TheMoonFox <foxdaz34000@gmail.com>
2016-01-28 01:43:37 +0100

    Obtention et parsing du token, debut de thread

--- doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/intraClient.java
+++ doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/intraClient.java
@@ -6,6 +6,9 @@ import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 
@@ -13,6 +16,8 @@ import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by Max on 26/01/2016.
+ * Added Token from json to string by Fox on 27/01/2016.
+ * Work on doInBackground json parsing by Fox on 28/01/2016.
  */
 
 public class intraClient extends AsyncTask {
@@ -37,21 +42,25 @@ public class intraClient extends AsyncTask {
         post("login", champs, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                System.out.println("Code de retour : " + statusCode);
-                System.out.println(Arrays.toString(headers));
+                System.out.println("[MAXDEBUG] Code de retour : " + statusCode);
+                System.out.println("[MAXDEBUG]" + Arrays.toString(headers));
                 String s = "";
                 try {
                     s = new String(responseBody, "ISO-8859-1");
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(s);
-
+                    System.out.println("[FOXDEBUG] JSON response to 'login' request = " + s);
+                    // PARSING JSON: REPONSE REQUETE "LOGIN" -> CLASS TOKEN (temporaire) //
+                    try {
+                        JSONObject jsonObject = new JSONObject(s);
+                        Token token = new Token(jsonObject.getString("token"));
+                        System.out.println("[FOXDEBUG] Parsed JSON Token = " + token.getToken());
+                    } catch (JSONException e) { e.printStackTrace(); }
+                    // ----------------------------------------------------------------- //
+                } catch (UnsupportedEncodingException e) { e.printStackTrace(); }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                System.out.println("Code de retour : " + statusCode);
+                System.out.println("[MAXDEBUG] Code de retour : " + statusCode);
                 System.out.println(Arrays.toString(headers));
                 System.out.println(Arrays.toString(responseBody));
             }
