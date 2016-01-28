aa2c0089fbe8a580199337a47c771c36ac17bc37
TheMoonFox <foxdaz34000@gmail.com>
2016-01-28 04:04:38 +0100

    Affichage image profil

--- doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/MainActivity.java
+++ doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/MainActivity.java
@@ -1,29 +1,43 @@
 package com.example.max.kikooworld;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
 
 import com.loopj.android.http.RequestParams;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imgView;
+    private Bitmap bmp;
+    final intraClient client = new intraClient(this);
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        final intraClient client = new intraClient();
         setContentView(R.layout.activity_main);
         final Button button = (Button) findViewById(R.id.loginButton);
+        imgView = (ImageView) findViewById(R.id.imageView);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 RequestParams rq = new RequestParams();
                 rq.put("login", ((EditText) findViewById(R.id.LoginScreenLoginTextField)).getText());
                 rq.put("password", ((EditText) findViewById(R.id.LoginScreenPasswordTextField)).getText());
-                client.login(rq);
+                String logi = String.valueOf(((EditText) findViewById(R.id.LoginScreenLoginTextField)).getText());
+                client.login(rq, logi);
             }
         });
+    }
+
+    public void setPhoto(String photoUrl)
+    {
+        // REVELE L'HORRIBLE VISAGE DE L'UTILISATEUR (voir INTRACLIENT) //
+        Picasso.with(getApplicationContext()).load(photoUrl).into((ImageView) findViewById(R.id.imageView));
+        System.out.println("[FOXDEBUG] Tentative de révéler l'horrible visage terminée.");
+        // ------------------------------------------------------------- //
     }
 }
--- doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/intraClient.java
+++ doceul_l/Kikooworld/app/src/main/java/com/example/max/kikooworld/intraClient.java
@@ -1,5 +1,6 @@
 package com.example.max.kikooworld;
 
+import android.graphics.Bitmap;
 import android.os.AsyncTask;
 
 import com.loopj.android.http.AsyncHttpClient;
@@ -22,8 +23,17 @@ import cz.msebera.android.httpclient.Header;
 
 public class intraClient extends AsyncTask {
     private static final String BASE_URL = "https://epitech-api.herokuapp.com/";
-
+    private String token;
+    private String photoUrl;
+    private String login_l;
     private static AsyncHttpClient client = new AsyncHttpClient();
+    private Bitmap photoBitmap;
+    private MainActivity mainActivity;
+
+    public intraClient(MainActivity mainActivity)
+    {
+        this.mainActivity = mainActivity;
+    }
 
     public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
         client.get(getAbsoluteUrl(url), params, responseHandler);
@@ -37,7 +47,7 @@ public class intraClient extends AsyncTask {
         return BASE_URL + relativeUrl;
     }
 
-    public static void login(RequestParams champs)
+    public void login(final RequestParams champs, final String login_str)
     {
         post("login", champs, new AsyncHttpResponseHandler() {
             @Override
@@ -45,16 +55,23 @@ public class intraClient extends AsyncTask {
                 System.out.println("[MAXDEBUG] Code de retour : " + statusCode);
                 System.out.println("[MAXDEBUG]" + Arrays.toString(headers));
                 String s = "";
+                login_l = login_str;
                 try {
                     s = new String(responseBody, "ISO-8859-1");
                     System.out.println("[FOXDEBUG] JSON response to 'login' request = " + s);
-                    // PARSING JSON: REPONSE REQUETE "LOGIN" -> CLASS TOKEN (temporaire) //
+                    // PARSING JSON: REPONSE REQUETE "LOGIN" //
                     try {
                         JSONObject jsonObject = new JSONObject(s);
-                        Token token = new Token(jsonObject.getString("token"));
-                        System.out.println("[FOXDEBUG] Parsed JSON Token = " + token.getToken());
+                        token = jsonObject.getString("token");
+                        System.out.println("[FOXDEBUG] Parsed JSON Token = " + token);
+                        // REQUETE PHOTO DANS LA FOULEE //
+                        RequestParams photoParams = new RequestParams();
+                        photoParams.add("token", token);
+                        photoParams.add("login", login_l);
+                        getFace(photoParams); // REQUETE PHOTO
+                        // ---------------------------- //
                     } catch (JSONException e) { e.printStackTrace(); }
-                    // ----------------------------------------------------------------- //
+                    // ------------------------------------- //
                 } catch (UnsupportedEncodingException e) { e.printStackTrace(); }
             }
 
@@ -65,6 +82,55 @@ public class intraClient extends AsyncTask {
                 System.out.println(Arrays.toString(responseBody));
             }
         });
+    }
+
+    public void getFace(RequestParams params)
+    {
+        // REQUETE PHOTO
+        get("photo", params, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                String response = "";
+                try {
+                    response = new String(responseBody, "ISO-8859-1");
+                    System.out.println("[FOXDEBUG] JSON response to 'photo' request = " + response);
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        photoUrl = jsonObject.getString("url");
+                        System.out.println("[FOXDEBUG] Parsed JSON Photo URL = " + photoUrl);
+                        mainActivity.setPhoto(photoUrl);
+                        //photoUrl = photoUrl + ".jpg";
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    // ------------------------------------- //
+                } catch (UnsupportedEncodingException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                System.out.println("[FOXDEBUG] Code retour 'requete photo' : " + statusCode);
+                System.out.println(Arrays.toString(headers));
+                System.out.println(Arrays.toString(responseBody));
+            }
+        });
+    }
+
+    public String getToken()
+    {
+        return (this.token);
+    }
+
+    public String getPhotoUrl()
+    {
+        return (this.photoUrl);
+    }
+
+    public Bitmap getPhotoBitmap()
+    {
+        return (this.photoBitmap);
     }
 
     @Override
